b8a081174d146fd53fb4c53670238a20c96f3571
deepbarankar <deepbaran97@gmail.com>
2026-01-13 06:04:52 +0530

    Adding initial Spring Boot basic setup

---
+++ src/test/java/com/org/parkinglotservice/ParkingLotServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.parkinglotservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingLotServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
